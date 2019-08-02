3d99da087e7bfe2370852dd8834c6f754660e022
Karol-Szykula <karol.szykula@gmail.com>
2019-08-02 21:47:51 +0200

    Test setup for finding users

---
+++ test/reading_test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Reading users out of the database', (done) => {
+    let joe;
+
+    beforeEach(() => {
+        joe = new User({ name: 'Joe' });
+        joe.save()
+            .then(() => { done(); })
+    });
+
+    it('finds all users with a name of Joe', (done) => {
+
+    });
+
+});
